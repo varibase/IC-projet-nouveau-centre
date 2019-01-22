317da989d63291e0177b446451893359644b195f
Nadine Triaire <nadine@perikan.net>
2019-01-22 18:58:09 +0100

    Ajout design carte verticale et retournement carte horizontale + suppression patch bloquer le contournement

--- resources/views/mycard.blade.php
+++ resources/views/mycard.blade.php
@@ -1,24 +1,45 @@
-<div class="container h-100">
-    <div class="row h-100">
+<div class="container">
+    <div class="row">
         <div class="col">
             <h1>@lang('pages.mycard.titre')</h1>
-            <div id="macarte">
+            <div id="macarte-portrait" class="h-100" >
 
-                <svg class="img-fluid"  viewBox="0 0 181.50416 286.27917" version="1.1" id="svg16">
+                <svg class="img-fluid" viewBox="0 0 181.50416 286.27917" version="1.1" id="svg16">
                     <defs id="defs10" />
-                        <g id="layer1" transform="translate(0,-10.720817)">
+                        
                         <g id="layer2">
                             <image
-                                y="11.287775"
-                                x="-0.037804745"
+                                y="2"
+                                x="0"
                                 id="image27"
-                                xlink:href="/img/fond-macarte.png"
+                                xlink:href="/img/fond-macarte-verticale.png"
                                 preserveAspectRatio="none"
                                 height="286.27917"
                                 width="181.50417" />
                         </g>
-                        <text x="74" y="30" style="fill: #000000; stroke: none;font-family:Arial; font-size: 8px;font-weight:bold;writing-mode: tb;text-transform:uppercase;">{{ $user->first_name}} {{ $user->last_name}}</text>
-                        <text x="33" y="153" text-anchor="middle" alignment-baseline="middle" style="fill: #000000; stroke: none;font-family: 'Libre Barcode 128 Text', cursive; font-size: 40px;writing-mode: tb;">{{ $user->card->card_number }}</text>
+                        <text x="90" y="215" text-anchor="middle" alignment-baseline="middle" style="fill: #000000; stroke: none;font-family:Arial; font-size: 8px;font-weight:bold;text-transform:uppercase;">{{ $user->first_name}} {{ $user->last_name}}</text>
+                        <text x="90" y="250" text-anchor="middle" alignment-baseline="middle" style="fill: #000000; stroke: none;font-family: 'Libre Barcode 128 Text', cursive; font-size: 38px;">{{ $user->card->card_number }}</text>
+                </svg>
+
+            </div>
+
+            <div id="macarte-landscape" class="w-100">
+
+                <svg class="img-fluid" viewBox="0 0 286.27917 181.50416" version="1.1" id="svg18">
+                    <defs id="defs12" />
+                        
+                        <g id="layer2">
+                            <image
+                                y="2"
+                                x="0"
+                                id="image27"
+                                xlink:href="/img/fond-macarte-paysage.png"
+                                preserveAspectRatio="none"
+                                height="181.50417"
+                                width="286.27917" />
+                        </g>
+                        <text x="20" y="110" style="fill: #000000; stroke: none;font-family:Arial; font-size: 8px;font-weight:bold;text-transform:uppercase;">{{ $user->first_name}} {{ $user->last_name}}</text>
+                        <text x="143" y="145" text-anchor="middle" alignment-baseline="middle" style="fill: #000000; stroke: none;font-family: 'Libre Barcode 128 Text', cursive; font-size: 40px;">{{ $user->card->card_number }}</text>
                 </svg>
 
             </div>
