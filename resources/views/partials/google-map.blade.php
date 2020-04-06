4aaae34022dcaa58bb28d11941961e6e6e9c2ffd
edouard_barbe <edouard.barbe@gmail.com>
2020-04-06 08:20:40 -0400

    Google Maps si pas d'offre

--- resources/views/partials/google-map.blade.php
+++ resources/views/partials/google-map.blade.php
@@ -1,5 +1,7 @@
 <div id="map" style="width:100%; height: 750px;">
+    @if(count($offers))
     {!! Mapper::render(0) !!}
+    @endif
 </div>
 <script type="text/javascript">
     function ShowOffer(idOffer) {
